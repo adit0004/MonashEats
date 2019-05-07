438862b1d42f0432ecc53bdf7f7df968f45e7ed0
Lyman3499 <49185014+Lyman3499@users.noreply.github.com>
2019-05-07 16:33:50 +1000

    Add files via upload

--- Admin.java
+++ Admin.java
@@ -1,26 +1,22 @@
-/**
- * Class for admin user
- *
- * @author Aditya Kumar
- * @version 5/5/2019
- */
 
+/**
+ * 
+ */
 public class Admin extends User
 {
     /**
-     * Default constructor should create a default administrator
+     * The non-default constructor of Admin class
      */
-    
+    public Admin(String newFirstName, String newLastName, String NewphoneNumber,String newEmail,String newPassword, String newAcountType)
+    {
+        super(newFirstName,newLastName,NewphoneNumber,newEmail,newPassword,newAcountType);
+    }
+
+    /**
+     * The default constructor of Admin class
+     */
     public Admin()
     {
-        super("Administrator", "", "", "admin1@monash.edu", "admin");
-    }
-    
-    /**
-     * Non-default constructor
-     */
-    public Admin(String newFirstName, String newLastName, String newPhoneNumber, String newEmailAddress, String newPassword)
-    {
-        super(newFirstName, newLastName, newPhoneNumber, newEmailAddress, newPassword);
+        super();
     }
 }
