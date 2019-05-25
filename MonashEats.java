7006615613535118a5f89bb9a88f4f415d018c9f
yupuli <49184904+yupuli@users.noreply.github.com>
2019-05-25 22:16:24 +1000

    MonashEats change

--- MonashEats.java
+++ MonashEats.java
@@ -13,6 +13,8 @@ public class MonashEats
     private ArrayList<User> userList;
     private RestaurantList restaurantList;
     private ShoppingCart shoppingCart;
+    public Boundary ui;
+    public RestaurantList rl;
 
     /**
      * Default constructor for MonashEats
@@ -30,10 +32,81 @@ public class MonashEats
         restaurantList = newRestaurantList;
         shoppingCart = newShoppingCart;
     }
-    
-    public static void main(String[] args)
-    {
-        MonashEats monashEats = new MonashEats();
+
+
+    public static void main(String[] args) {
+        MonashEats me = new MonashEats();
+        ShoppingCart cart = new ShoppingCart();
+        me.checkOut(cart);
+    }
+
+    public void checkOut(ShoppingCart cart) {
+        ui.checkOutPage();//show the check out page
+        MonashEats me = new MonashEats();       
+        ShoppingCart carts = new ShoppingCart();       
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        switch (input) {
+            case 1:
+                me.redeemCoupon(carts);
+                break;
+            case 2:
+                me.afterPayment(1);
+                break;
+            case 3:
+                me.afterPayment(2);
+                break;
+            case 4:
+                checkOut(carts);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void redeemCoupon(ShoppingCart cart) {
+        ShoppingCart scart = cart;
+        ui.couponPage();//coupon page
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        if (input == 1) {
+            String coupon = sc.nextLine();
+            if (coupon.toUpperCase().equals("COUPON")) {
+                double totalPrice = scart.getTotalPrice();
+                totalPrice = totalPrice - 5.0;
+                scart.setTotalPrice(totalPrice);
+                boolean flag = true;
+                ui.list(true);//to add coupon to create new page with coupon and change the total price
+                ui.paymentPage();//after enter coupon code, show the payment option: card or cash
+                int op = sc.nextInt();
+                switch (op) {
+                    case 1:afterPayment(1);//show the list or the receipt for the customer after pay by card
+                        break;
+                    case 2:afterPayment(2);//pay by cash
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }else{
+            checkOut(scart);//go back to the previous page
+        }
+    }
+
+    private void afterPayment(int i) {
+        ui.afterPaymentPage();
+        Scanner sc = new Scanner(System.in);
+        int ops =sc.nextInt();
+        boolean flag = false;
+        do{
+            if(ops==1){
+                ui.ratingPage();
+            }else{
+                ui.showReciptPage();
+                int j = sc.nextInt();
+                rl.rateRestaurant();
+            }
+        }while(flag);
     }
 
     /**
